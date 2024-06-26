1e0307c09201ff0805b8cfd54ec6a96618804481
Guillaume Renault <guillaume@ektos.ai>
2024-06-26 19:44:30 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -36,8 +36,8 @@ pipe = pipeline(
     "automatic-speech-recognition",
     model="openai/whisper-large-v3",
     torch_dtype=torch.float16,
-    device="cuda:0",
-    model_kwargs={"attn_implementation": "flash_attention_2"} if is_flash_attn_2_available() else {"attn_implementation": "sdpa"},
+    device="cuda:0"
+    #model_kwargs={"attn_implementation": "flash_attention_2"} if is_flash_attn_2_available() else {"attn_implementation": "sdpa"},
 
 )
 
